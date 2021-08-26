2ff2c9f21bb9b38d5384ca0051a0ccaf6d4dbc31
GlobeScope <GlobeScope@DESKTOP-GNJN415>
2021-08-26 02:28:37 -0700

    Add project files.

---
+++ EmployeeApiData/Controllers/DepartmentController.cs
@@ -0,0 +1,137 @@
+﻿using EmployeeApiData.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeApiData.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select DepartmentId , DepartmentName from dbo.Department";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _config.GetConnectionString("EmployeeAppApiData");
+            SqlDataReader myReader;
+            using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myComand = new SqlCommand(query, myConn))
+                {
+                    myReader = myComand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myConn.Close();
+                }
+
+            }
+            return new JsonResult(table);
+
+        }
+
+
+        [HttpPost]
+        public JsonResult Post(Department dep)
+        {
+            string query = @"
+                        insert into dbo.Department (DepartmentName) values
+                                                    (@DepartmentName);
+                        
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _config.GetConnectionString("EmployeeAppApiData");
+            SqlDataReader myReader;
+            using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                {
+                    myCommand.Parameters.AddWithValue("@DepartmentName", dep.DepartmentName);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+
+        [HttpPut]
+        public JsonResult Put(Department dep)
+        {
+            string query = @"update dbo.Department
+                            set DepartmentName = @DepartmentName
+                            where DepartmentId = @DepartmentId";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _config.GetConnectionString("EmployeeAppApiData");
+            SqlDataReader myReader;
+            using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myComand = new SqlCommand(query, myConn))
+                {
+                    myComand.Parameters.AddWithValue("@DepertmentId", dep.DepartmentId);
+                    myComand.Parameters.AddWithValue("@DepertmentName", dep.DepartmentName);
+                    myReader = myComand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myConn.Close();
+                }
+
+            }
+            return new JsonResult(table);
+
+        }
+
+        [HttpDelete]
+        public JsonResult Delete(int id)
+        {
+            string query = @"delete from dbo.Department
+                            where DepartmentId = @DepartmentId";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _config.GetConnectionString("EmployeeAppApiData");
+            SqlDataReader myReader;
+            using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myComand = new SqlCommand(query, myConn))
+                {
+                    myComand.Parameters.AddWithValue("@DepertmentId", id);
+
+                    myReader = myComand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myConn.Close();
+                }
+
+            }
+            return new JsonResult(table);
+
+
+        }
+    }
+}
